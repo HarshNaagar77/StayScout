5b59b001fc9fa3824b76d1287dc5a61010f6b554
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-12 23:15:37 +0530

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const stripe = Stripe('sk_test_51Pg8asRrdvF7ebjyIBJzqgNS8CcEJaIr2PIciKw434H4iGt6
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(cors({
-  origin: 'https://stayscout-1.onrender.com/',
+  origin: 'https://stayscout-1.onrender.com',
   credentials: true
 }));  
 app.use(express.json());
