b10d5ed137eba82b36d0c2882a5e455caf263a7d
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-12 22:24:52 +0530

    Update

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/StayScout')
+mongoose.connect('mongodb+srv://HarshDB123:harsh9311@practicedb.wl8ctoe.mongodb.net/StayScout?retryWrites=true&w=majority&appName=practiceDB')
 
 const userSchema = new mongoose.Schema({
     username: {
